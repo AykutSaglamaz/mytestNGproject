41f188fff3280533224239ebf29894dc5ff80949
AykutSaglamaz <gulerrahim@gmail.com>
2023-01-01 05:42:39 -0500

    update2

--- src/test/java/com/tests/Day09_C17_WebTables.java
+++ src/test/java/com/tests/Day09_C17_WebTables.java
@@ -13,65 +13,82 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
-public class DAy09_C17_WebTables {
+public class Day09_C17_WebTables {
+//    When kullanici application'da HotelRoom sayfasina gider
+        // Log in https://www.carettahotel.com/
+        // Hotel Management'e Click yapar
+        // Hotel Rooms'a Click yapar
+//    test method: entireTable() ve tum header'lari yazdir
+//    test method: printRows() ve tum row'lari ile 4 row'daki elementi yazdir
+//    test method: printCells() ve table body'dek' cells'lerin toplam sayilarini yazdir ve tum cell'leri yazdir
+//    test method: printColumns() ve columns'larin toplam sayisini yazdir ve 5. column yazdir
+//    test method: printData(int row, int column); Bu method veriline cell'i yazdirmali
+//    ornek : printData(2,3); 2. row ve 3 column yazdirmali
+
+
     Day09_C12_LoginPage loginPage;
     Day09_C13_DefaultPage defaultPage;
     Day09_C16_HotelRoomPage hotelRoomsPage;
-    //    When user goes to HotelRoom page on the application
+    //    When kullanici application'da HotelRoom sayfasina gider
     public void setUp(){
         Day09_C4_Driver.getDriver().get(Day09_C6_ConfigReader.getProperty("app_url_login"));
         loginPage= new Day09_C12_LoginPage();
+        //private isterse
+        loginPage.advancedLink.click();
+        loginPage.proceedLink.click();
 
         loginPage.username.sendKeys(Day09_C6_ConfigReader.getProperty("manager_username"));
         loginPage.password.sendKeys(Day09_C6_ConfigReader.getProperty("manager_password"));
         loginPage.loginButton.click();
 
-        //asserting login success
+        //login basarili oldugunu assert et
         defaultPage= new Day09_C13_DefaultPage();
         Assert.assertTrue(defaultPage.addUserButton.isDisplayed());
 
-        //Click on Hotel Management
+        //Hotel Management'e Click yapar
         defaultPage.hotelManagementTab.click();
 
-        //Click on Hotel Rooms
+        //Hotel Rooms'a Click yapar
         defaultPage.hotelRoomsTab.click();
 
-        //Click on Add Hotel Room
+        //Add Hotel Room'a click yapar
         hotelRoomsPage=new Day09_C16_HotelRoomPage();
     }
+
+//    test method: entireTable() ve tum header'lari yazdir
     @Test(groups = "regression-group-1")
     public void entireTable(){
         setUp();
-//        Create a test method: entireTable() and print all of headers
         System.out.println("*Entire Table*");
         System.out.println("*Table Body*");
+
         WebElement tableBody = Day09_C4_Driver.getDriver().findElement(By.xpath("//table//tbody"));
         System.out.println(tableBody.getText());
         List<WebElement> allHeaders = Day09_C4_Driver.getDriver().findElements(By.xpath("//th"));
         for (WebElement eachHeader : allHeaders){
             System.out.println(eachHeader.getText());
         }
-//        Create a test method: printCells() and a the total number of cells in the table body and print all of the cells
-//        Create a test method: printColumns() and print Find the total number of columns and Print the elements of the 5th column
-//        Create a test method: printData(int row, int column); This method should print the given cell. Example: printData(2,3); should print 2nd row,3rd column
     }
+
+//    test method: printRows() olustur ve row sayisini ve 4. row'daki elementi yazdir
     @Test(groups = "regression-group-1")
     public void printRows(){
         setUp();
-        //        Create a test method: printRows() and Print all of the rows and print the element s on the 4th row
         System.out.println("*Print Rows*");
         List<WebElement> allRows = Day09_C4_Driver.getDriver().findElements(By.xpath("//tbody//tr"));
-        //row numbers
+
+        //row sayisi
         int rowNum=1;
         for (WebElement eachRow : allRows){
             System.out.println("Row Number "+rowNum + " => "+eachRow.getText());
             rowNum++;
         }
-        //Print row4 only
+        //yalnizca row4 yazdir
         WebElement row4 = Day09_C4_Driver.getDriver().findElement(By.xpath("//tbody//tr[4]"));
         System.out.println("ROW 4 => "+row4.getText());
     }
-    //    Create a test method: printCells() and a the total number of cells in the table body and print all of the cells
+
+//   test method: printCells() ve table body'dek' cells'lerin toplam sayilarini yazdir ve tum cell'leri yazdir
     @Test
     public void printCells(){
         setUp();
@@ -84,15 +101,17 @@ public class DAy09_C17_WebTables {
             cellNum++;
         }
     }
-    //    Create a test method: printColumns() and print Find the total number of columns and Print the elements of the 5th column
+
+//    test method: printColumns() ve columns'larin toplam sayisini yazdir ve 5. column yazdir
     @Test
     public void printColumns(){
         setUp();
-        //The number of column equals to the number of th
+        //columns'larin toplam sayisini yazdir
         List<WebElement> allHeaders = Day09_C4_Driver.getDriver().findElements(By.xpath("//th"));
         int numberOfColumn = allHeaders.size();
         System.out.println("There are "+numberOfColumn + " column.");
-        //       Print the elements of the 5th column
+
+        // 5. column yazdir
         List<WebElement> column5Data = Day09_C4_Driver.getDriver().findElements(By.xpath("//table//tbody//tr//td[5]"));
         int columnNum=1;
         for (WebElement eachColumn : column5Data){
@@ -100,9 +119,10 @@ public class DAy09_C17_WebTables {
             columnNum++;
         }
     }
-    //    Create a test method: printData(int row, int column);
-//    This method should print the given cell.
-//    Example: printData(2,3); should print 2nd row,3rd column
+
+//    test method: printData(int row, int column); Bu method veriline cell'i yazdirmali
+//    ornek : printData(2,3); 2. row ve 3 column yazdirmali
+
     @AfterMethod
     public void tearDown(){
         Day09_C4_Driver.closeDriver();
